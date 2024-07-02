90d41e822a5f46773c7d4bcd853b078c76ea5468
hoamx <hoamx.work@gmail.com>
2024-07-03 04:46:37 +0700

    feat: create form login

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -3,29 +3,29 @@
 import { useRouter } from "next/navigation";
 
 interface LoginButtonProps {
-  children: React.ReactNode;
-  mode?: "modal" | "redirect";
-  asChild?: boolean;
+	children: React.ReactNode;
+	mode?: "modal" | "redirect";
+	asChild?: boolean;
 }
 
 const LoginButton = ({
-  children,
-  mode = "redirect",
-  asChild,
+	children,
+	mode = "redirect",
+	asChild,
 }: LoginButtonProps) => {
-  const router = useRouter();
-  const onClick = () => {
-    router.push("/auth/login");
-  };
+	const router = useRouter();
+	const onClick = () => {
+		router.push("/auth/login");
+	};
 
-  if (mode === "modal") {
-    return <span>TODO: Implement modal</span>;
-  }
-  return (
-    <span onClick={onClick} className="cursor-pointer">
-      {children}
-    </span>
-  );
+	if (mode === "modal") {
+		return <span>TODO: Implement modal</span>;
+	}
+	return (
+		<span onClick={onClick} className="cursor-pointer">
+			{children}
+		</span>
+	);
 };
 
 export default LoginButton;
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,16 +1,93 @@
+"use client";
+
 import CardWrapper from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import FormError from "../form-error.";
+import FormSuccess from "../form-success";
 
 const LoginForm = () => {
-  return (
-    <CardWrapper
-      headerLabel="Welcome back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
-      showSocial
-    >
-      Login Form
-    </CardWrapper>
-  );
+	const form = useForm<z.infer<typeof LoginSchema>>({
+		resolver: zodResolver(LoginSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+		console.log('🟢====>values', values);
+	}
+	return (
+		<CardWrapper
+			headerLabel="Welcome back"
+			backButtonLabel="Don't have an account?"
+			backButtonHref="/auth/register"
+			showSocial
+		>
+			<Form {...form}>
+				<form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+					<div className="space-y-4">
+						<FormField
+							control={form.control}
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											placeholder="john.doe@example.com"
+											type="email"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											placeholder="******"
+											type="password"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<FormError message="" />
+					<FormSuccess message=""  />
+					<Button
+					type="submit"
+					className="w-full"
+					>
+						Login
+					</Button>
+				</form>
+			</Form>
+		</CardWrapper>
+	);
 };
 
 export default LoginForm;
