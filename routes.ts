ea5a6094123d57985e91c14953334b412abfb638
hoamx <hoa.mx@kiotviet.com>
2024-07-03 15:21:31 +0700

    feat: add middleware with specific routes

---
+++ routes.ts
@@ -0,0 +1,23 @@
+/**
+ * The routes do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * The routes do require authentication
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for API authentication routes
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settins";
