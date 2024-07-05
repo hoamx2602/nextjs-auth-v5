d4942c3a276556755d8acc53c6b5f19442e47cc6
hoamx <hoamx.work@gmail.com>
2024-07-06 00:59:10 +0700

    feat: handle reset password page

---
+++ components/auth/new-password-form.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { login } from "@/actions/login";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { LoginSchema, NewPasswordSchema, ResetSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import FormError from "../form-error.";
+import FormSuccess from "../form-success";
+import { Button } from "../ui/button";
+import CardWrapper from "./card-wrapper";
+import { reset } from "@/actions/reset";
+import { useSearchParams } from "next/navigation";
+import { newPassword } from "@/actions/new-password";
+
+const NewPasswordForm = () => {
+	const searchParams = useSearchParams();
+	const token = searchParams.get("token");
+	const [isPending, startTransition] = useTransition();
+	const [error, setError] = useState<string | undefined>("");
+	const [success, setSuccess] = useState<string | undefined>("");
+
+	const form = useForm<z.infer<typeof NewPasswordSchema>>({
+		resolver: zodResolver(NewPasswordSchema),
+		defaultValues: {
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+		setError("");
+		setSuccess("");
+		startTransition(() => {
+			newPassword(values, token).then((data) => {
+				setError(data?.error);
+				setSuccess(data?.success);
+			});
+		});
+	};
+	return (
+		<CardWrapper
+			headerLabel="Enter a new password"
+			backButtonLabel="Back to login"
+			backButtonHref="/auth/login"
+		>
+			<Form {...form}>
+				<form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+					<div className="space-y-4">
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											disabled={isPending}
+											placeholder="******"
+											type="password"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<FormError message={error} />
+					<FormSuccess message={success} />
+					<Button disabled={isPending} type="submit" className="w-full">
+						Reset password
+					</Button>
+				</form>
+			</Form>
+		</CardWrapper>
+	);
+};
+
+export default NewPasswordForm;
